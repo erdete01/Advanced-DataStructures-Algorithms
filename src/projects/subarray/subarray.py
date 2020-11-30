a99dd72ff37f232c4ce50b6dd094b0de5d48f4ac
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-11-30 15:45:09 -0600

    P0 done

--- src/projects/subarray/subarray.py
+++ src/projects/subarray/subarray.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 """Subarray"""
 
-
+# Time Complexity --> O(n)
 def kadane(array: list) -> int:
     """Implementation of Kadane's algorithm"""
-    best_sum = 0  # or: float('-inf')
-    current_sum = 0
-    for x in array:
-        current_sum = max(0, current_sum + x)
-        best_sum = max(best_sum, current_sum)
+    # If there is nothing, there will be no subarrays and return 0
+    if len(array) == 0:
+        return 0
+    # else get the first items and then iterate from the index 1
+    best_sum = array[0]  # or: float('-inf')
+    current_sum = array[0]
+    for i in range(1, len(array)):
+        # max of both current array[i] and array[i-1] gives my current_sum
+        current_sum = max(array[i], current_sum + array[i])
+        # same as max(current_sum, best_sum). Update the best_sum
+        if current_sum > best_sum:
+            best_sum = current_sum
     return best_sum
         
 
