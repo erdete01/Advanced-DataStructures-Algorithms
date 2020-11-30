557636e954f98c1a53e773b660f2a10f727f89e6
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-11-30 14:51:50 -0600

    Project 0 Done

--- src/projects/subarray/subarray.py
+++ src/projects/subarray/subarray.py
@@ -4,8 +4,13 @@
 
 def kadane(array: list) -> int:
     """Implementation of Kadane's algorithm"""
-    raise NotImplementedError
-
+    best_sum = 0  # or: float('-inf')
+    current_sum = 0
+    for x in array:
+        current_sum = max(0, current_sum + x)
+        best_sum = max(best_sum, current_sum)
+    return best_sum
+        
 
 def main():
     """This is the main function"""
