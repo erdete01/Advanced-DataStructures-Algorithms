cb7e96a28833d5b5dfa740f5c61f09ca11f5b65f
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-12-02 18:47:07 -0600

    read_file done

--- src/projects/classy/classifier.py
+++ src/projects/classy/classifier.py
@@ -12,6 +12,8 @@ def classify(people: dict) -> List[str]:
     
     Return the ordered (highest to lowest) list
     """
+    # ["Beaver", "Elephant", "Aardvark", "Cheetah", "Dolphin"],
+    # Why Dolphine is After Cheetah. Not sure. 
     raise NotImplementedError
 
 
@@ -21,10 +23,16 @@ def read_file(filename: str) -> Dict[str, str]:
 
     Return the {person: class} mapping
     """
-    raise NotImplementedError
-
-
-
+    # Time Complexity O(n^2) 
+    # because python find method takes linear 
+    # and for loop adds one more loop. 
+    # Therefore, it's O(n^2)
+    f = open(filename, "r")
+    myDictionary = dict()
+    for x in f: 
+        myDictionary[x[0: x.find(':')]] = x[x.find(':') + 1 : x.find('class')]
+    return myDictionary
+    
 def main():
     """Entry point"""
     people = read_file("data/projects/classy/classy01.txt")
