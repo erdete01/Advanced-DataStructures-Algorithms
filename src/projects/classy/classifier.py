54b886136f910a573eea25cd9323288595ca6962
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-12-02 21:33:39 -0600

    Test2

--- src/projects/classy/classifier.py
+++ src/projects/classy/classifier.py
@@ -14,7 +14,31 @@ def classify(people: dict) -> List[str]:
     """
     # ["Beaver", "Elephant", "Aardvark", "Cheetah", "Dolphin"],
     # Why Dolphine is After Cheetah. Not sure. 
-    raise NotImplementedError
+    # My Brute Force Solution might be iterate through the dictionary
+    # And at the same time, I will also iterate through myList 3 times. It will be 
+    # Time Complexity O(n^2)
+    # people = {"Ant": "upper", "Bee": "middle", "Cat": "lower"}
+    myList = {}
+    finalList = []
+    for aKey, aValue in people.items():
+        if len(myList) > 0:
+            for x, y in myList.items():
+                if y == "upper":
+                    myList[x] y
+                elif y =="middle":
+                    if y == "upper":
+                        myList.insert(i-1,  aValue)
+                        i += 1
+                    else:
+                        myList.append(aValue)
+                        i += 1
+                else:
+                    myList.insert(i-1, aValue)
+                    i += 1
+        else:
+            myList[aKey] = aValue
+    print(myList)
+    
 
 
 def read_file(filename: str) -> Dict[str, str]:
