792c7b2e014b765dc1c624104ce21fbe4be64c9a
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-12-07 20:51:14 -0600

    Exercise 2 done

--- src/exercises/hashing/test.py
+++
@@ -1,7 +0,0 @@
-def hash_mid_sqr(key, size):
-    """Find hash using mid-square method"""
-    myNum = 0
-    for i in range(len(key)):
-        myNum += ord(key[i])
-    print(myNum % size)
-print(hash_mid_sqr("coronavirus", 7))
