a1edded1d3a55cb33c6bc4f9e1079ff04c0f594c
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-12-08 21:02:20 -0600

    Exercise 2 Updated

--- src/exercises/hashing/hashing.py
+++ src/exercises/hashing/hashing.py
@@ -15,15 +15,135 @@ def hash_mid_sqr(key: int, size: int) -> int:
 
 def hash_folding(key: int, size: int) -> int:
     """Find hash using folding method"""
-    myNumbers = ''
-    for i in key.split('-'):
-        myNumbers += i
-    converting = [str(myNumbers[i-1]) + str(myNumbers[i]) \
-         for i in range(1, len(myNumbers), 2)]
-    totalNum = 0
-    for i in converting:
-        totalNum += int(i)
-    return (totalNum % size)
+    """Find hash using folding method"""
+    if '-' in key:
+        myNumbers = ''
+        for i in key.split('-'):
+            myNumbers += i
+        if int(len(myNumbers)) % 2 != 0:
+            converting = []
+            for i in range(1, len(myNumbers), 2):
+                converting.append((str(myNumbers[i-1]) + str(myNumbers[i])))
+            converting.append(myNumbers[-1])
+        else:
+            converting = []
+            for i in range(1, len(myNumbers), 2):
+                converting.append((str(myNumbers[i-1]) + str(myNumbers[i])))
+        totalNum = 0
+        for i in converting:
+            totalNum += int(i)
+        return (totalNum % size)
+    if '/' in key:
+        myNumbers = ''
+        for i in key.split('/'):
+            myNumbers += i
+        if int(len(myNumbers)) % 2 != 0:
+            converting = []
+            for i in range(1, len(myNumbers), 2):
+                converting.append((str(myNumbers[i-1]) + str(myNumbers[i])))
+            converting.append(myNumbers[-1])
+        else:
+            converting = []
+            for i in range(1, len(myNumbers), 2):
+                converting.append((str(myNumbers[i-1]) + str(myNumbers[i])))
+        totalNum = 0
+        for i in converting:
+            totalNum += int(i)
+        return (totalNum % size)
+    if '.' in key:
+        myNumbers = ''
+        for i in key.split('.'):
+            myNumbers += i
+        if int(len(myNumbers)) % 2 != 0:
+            converting = []
+            for i in range(1, len(myNumbers), 2):
+                converting.append((str(myNumbers[i-1]) + str(myNumbers[i])))
+            converting.append(myNumbers[-1])
+        else:
+            converting = []
+            for i in range(1, len(myNumbers), 2):
+                converting.append((str(myNumbers[i-1]) + str(myNumbers[i])))
+        totalNum = 0
+        for i in converting:
+            totalNum += int(i)
+        return (totalNum % size)
+    if '(' in key or ')' in key:
+        i = 0
+        myList = []
+        while i < len(key):
+            if len(myList) == 0:
+                myList.append(key[i])
+                i += 1
+            else:
+                if myList[-1] == '(':
+                    myList.pop()
+                    
+                elif myList[-1] == ')':
+                    myList.pop()
+                    
+                elif myList[-1] == ' ':
+                   myList.pop()
+                else:
+                    myList.append(key[i])
+                    i += 1
+        myNumbers = "" 
+        myNumbers = (myNumbers.join(myList)) 
+        if int(len(myNumbers)) % 2 != 0:
+            converting = []
+            for i in range(1, len(myNumbers), 2):
+                converting.append((str(myNumbers[i-1]) + str(myNumbers[i])))
+            converting.append(myNumbers[-1])
+        else:
+            converting = []
+            for i in range(1, len(myNumbers), 2):
+                converting.append((str(myNumbers[i-1]) + str(myNumbers[i])))
+        totalNum = 0
+        for i in converting:
+            totalNum += int(i)
+        return (totalNum % size)
+
+    if key.isdigit():
+        if int(len(key)) % 2 != 0:
+            converting = []
+            for i in range(1, len(key), 2):
+                converting.append((str(key[i-1]) + str(key[i])))
+            converting.append(key[-1])
+        else:
+            converting = []
+            for i in range(1, len(key), 2):
+                converting.append((str(key[i-1]) + str(key[i])))
+        totalNum = 0
+        for i in converting:
+            totalNum += int(i)
+        return (totalNum % size)
+    else:
+        myNumbers = [i.strip() for i in key.split(',')]
+        myList = []
+        for i in myNumbers:
+            if " " in i:
+                a = i.split(' ')
+                for x in a:
+                    myList.append(x)
+            else:
+                myList.append(i)
+        myNum = ''
+        for i in myList:
+            if i.isdigit():
+                myNum += str(i)
+        
+        if int(len(myNum)) % 2 != 0:
+            converting = []
+            for i in range(1, len(myNum), 2):
+                converting.append((str(myNum[i-1]) + str(myNum[i])))
+            converting.append(myNum[-1])
+        else:
+            converting = []
+            for i in range(1, len(myNum), 2):
+                converting.append((str(myNum[i-1]) + str(myNum[i])))
+        totalNum = 0
+        for i in converting:
+            totalNum += int(i)
+        return (totalNum % size)
 
 
 def hash_str(key: str, size: int) -> int:
