a154de80ab92d755b90adfbeb25d4ba05be39b74
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-12-14 10:49:35 -0600

    Tree Exercise Test1

--- src/exercises/trees/traversal.py
+++ src/exercises/trees/traversal.py
@@ -4,7 +4,14 @@
 
 def get_preorder(inorder: str, postorder: str) -> str:
     """Return pre-order traversal of a tree based on its in-order and post-order traversals"""
-    raise NotImplementedError
+    # This will be always the node
+    theNode = postorder[-1]
+    # Find theNode in inorder string
+    sub = inorder.find(theNode)
+    leftTree = inorder[0: sub]
+    rightTree = inorder[sub+1::]
+    # Feel like I shoud use recursion
+    return theNode+ leftTree+rightTree
 
 
 def main():
