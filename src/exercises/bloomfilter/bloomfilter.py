84e61274c105721f8565f4701dece59dc65bacc9
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-12-22 15:29:52 -0600

    ...

--- src/exercises/bloomfilter/bloomfilter.py
+++ src/exercises/bloomfilter/bloomfilter.py
@@ -8,11 +8,13 @@ from zlib import crc32
 class BloomFilter:
     def __init__(self, size: int = 11, k: int = 3):
         """Initialize the filter"""
-        raise NotImplementedError
+        self._k = k
+        self._filter = [False] * size
+        
 
     def _hash(self, word: str) -> tuple:
         """Return a tuple of k indices"""
-        raise NotImplementedError
+        return tuple((crc32(bytes(f"{word}{i}", "utf8")) % len(self._filter) for i in range(self._k)))
 
     def add(self, word: str):
         """Add a dictionary word to the filter"""
@@ -24,8 +26,8 @@ class BloomFilter:
 
     def __str__(self):
         """Return string representation of the filter"""
-        return str(self._filter)
+        pass
 
     def __len__(self):
         """Return size of the filter"""
-        return len(self._filter)
+        pass
