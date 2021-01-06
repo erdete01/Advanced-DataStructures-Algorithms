4c1c83702391f2bbe5151eac30fe41f9b0d2a8a2
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-01-05 22:47:53 -0600

    Project Started and Exercise Stuck

--- src/projects/anomalycounter/anomalycounter.py
+++ src/projects/anomalycounter/anomalycounter.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 """
 Anomaly counter
-@author: 
+@author: Temuulen Erdenebulgan
 """
 
 
 def count(filename: str) -> int:
     """Count number of anomalies/blobs in an image"""
-    raise NotImplementedError
+    myList = []
+    f = open(filename, "r")
+    for x in f: myList.append(x.replace("\n",""))
+    print(myList)
+    for i in myList:
+        # Only Checking the first myList[0], not dynamic.
+        if i == len(i)*"*":
+            return 1
+        for k in i:
+            if "*" not in k: return 0
+            else:
+                pass
+                # Check East, West, South, and North 
+                # Maybe convert it into a while loop
